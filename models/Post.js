eece97c52bde8145ee7286c3b2496be6451acc64
Jon Ryan <j.p.ryan13@gmail.com>
2020-04-05 14:02:33 -0500

    View Posts by Author

--- models/Post.js
+++ models/Post.js
@@ -44,14 +44,9 @@ Post.prototype.create = function() {
   })
 }
 
-Post.findSinglePostById = function (id) {
+Post.reusablePostQuery = function (uniqueOperations) {
     return new Promise(async function (resolve, reject) {
-        if (typeof(id) != "string" || !ObjectID.isValid(id)) {
-            reject()
-            return
-        }
-        let posts = await postsCollection.aggregate([
-            {$match: {_id: new ObjectID(id)}},
+        let aggOperations = uniqueOperations.concat([
             {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
             {$project: {
                title: 1,
@@ -59,7 +54,8 @@ Post.findSinglePostById = function (id) {
                createdDate: 1,
                author: {$arrayElemAt: ["$authorDocument", 0]} 
             }}
-        ]).toArray()
+        ])
+        let posts = await postsCollection.aggregate(aggOperations).toArray()
         // clean up author property in each post object
         posts = posts.map(function (post) {
             post.author = {
@@ -68,6 +64,20 @@ Post.findSinglePostById = function (id) {
             }
             return post
         })
+        resolve(posts)
+    })
+}
+
+Post.findSinglePostById = function (id) {
+    return new Promise(async function (resolve, reject) {
+        if (typeof(id) != "string" || !ObjectID.isValid(id)) {
+            reject()
+            return
+        }
+        
+        let posts = await Post.reusablePostQuery([
+            {$match: {_id: new ObjectID(id)}}
+        ])
 
         if (posts.length) {
             resolve(posts[0])
@@ -75,6 +85,13 @@ Post.findSinglePostById = function (id) {
             reject()
         }
     })
+}
+
+Post.findByAuthorId = function(authorId) {
+    return Post.reusablePostQuery([
+        {$match: {author: authorId}},
+        {$sort: {createdDate: -1}} // decending order
+    ])
 }
 
 module.exports = Post
