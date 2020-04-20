2a83e15498b5bcbd415f56cc07e56dd0f09691f4
Jon Ryan <j.p.ryan13@gmail.com>
2020-04-20 10:35:24 -0500

    follower/following screens and logic

--- models/Follow.js
+++ models/Follow.js
@@ -98,4 +98,41 @@ Follow.getFollowersById = function (id) {
 	})
 }
 
+Follow.getFollowingById = function (id) {
+	return new Promise(async (resolve, reject) => {
+		try {
+			let followers = await followsCollection.aggregate([
+				{$match: {authorId: id}},
+				{$lookup: {from: "users", localField: "followedId", foreignField: "_id", as: "userDoc"}},
+				{$project: {
+					username: {$arrayElemAt: ["$userDoc.username", 0]},
+					email: {$arrayElemAt: ["$userDoc.email", 0]}
+				}}
+			]).toArray()
+			follower = followers.map(function (follower) {
+				// create a user
+				let user = new User(follower, true)
+				return {username: follower.username, avatar: user.avatar}
+			})
+			resolve(followers)
+		} catch (err) {
+			reject(err)
+		}
+	})
+}
+
+Follow.countFollowersById = function(id) {
+  return new Promise(async (resolve, reject) => {
+    let followerCount = await followsCollection.countDocuments({followedId: id})
+    resolve(followerCount)
+  })
+}
+
+Follow.countFollowingById = function(id) {
+  return new Promise(async (resolve, reject) => {
+    let count = await followsCollection.countDocuments({authorId: id})
+    resolve(count)
+  })
+}
+
 module.exports = Follow
